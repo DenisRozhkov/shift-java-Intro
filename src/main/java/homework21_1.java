053630bdbe5870f329e2e8b87b2fde5406e9d34c
Denis <rozhkovdenis2003@gmail.com>
2025-11-15 02:35:24 +0700

    удаление

--- src/main/java/homework21_1.java
+++ src/main/java/homework21_1.java
@@ -1,18 +1,1 @@
-package main.java;
 
-public class homework21_1 {
-    void main() {
-        System.out.println("P\t\tQ\t\tP AND Q\tP OR Q\tP XOR Q\tNOT P");
-        boolean p = false;
-        boolean q = false;
-        System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (!p));
-        p = true;
-        System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (!p));
-        p = false;
-        q = true;
-        System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (!p));
-        p = true;
-        q = true;
-        System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (!p));
-    }
-}
