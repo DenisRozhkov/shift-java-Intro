6e33a5761a5fd04b182ae222104ef71ebf42dfe1
Denis <rozhkovdenis2003@gmail.com>
2025-11-15 02:33:18 +0700

    черновик дз22

---
+++ src/main/java/homework22.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class homework22 {
+    void main() {
+        /*for (int i = 1; i < 11;i=i+2 ) {
+            System.out.println("4*"+i +"="  + (4*i));
+        }*/
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                System.out.println(i*j);
+            }
+        }
+    }
+}
