d7d5ba774025ab886c98d07af600420ebc8a00c9
Denis <rozhkovdenis2003@gmail.com>
2025-11-21 21:57:31 +0700

    дз 231

--- src/main/java/SymbolReplacement.java
+++ src/main/java/SymbolReplacement.java
@@ -2,26 +2,22 @@ package main.java;
 
 import java.util.Scanner;
 
-public class homework23 {
+public class SymbolReplacement {
     static void main() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите исходную строку: ");
         String string = scanner.nextLine();
         System.out.print("Введите символ: ");
         String symbol = scanner.nextLine();
-String modifiedString ;
-        modifiedString = string.replace("  " , " ");
-        modifiedString =modifiedString.replace(symbol , " ");
-        modifiedString = modifiedString.replace("  " , " ");
+        String modifiedString;
+        modifiedString = string.replace("  ", " ");
+        modifiedString = modifiedString.replace(symbol, " ");
+        modifiedString = modifiedString.replace("  ", " ");
         String finalString = string + modifiedString;
         System.out.print(finalString);
         if (string.equals(modifiedString)) {
             System.out.println("\nСтрока не изменена");
         }
-
-       /* System.out.println(string.replace("  " , " "));
-        System.out.println(string.replace(symbol , " "));
-        System.out.println(string.replace("  " , " "));*/
     }
 }
 
